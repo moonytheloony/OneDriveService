a1531a4fbe423ab1dc2f6624a21725ef265768af
moonytheloony <rahulrai@live.com>
2015-10-12 19:45:30 +0530

    OneDrive Service

---
+++ OneDriveService/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using OneDriveService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OneDriveService.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        private static OneDriveConnectorRequest[] files;
+
+        // GET api/values
+        public string Get()
+        {
+            var data = string.Empty;
+            foreach (var value in files)
+            {
+                data += "File Name: " + value.FileName + ", ";
+            }
+
+            return data;
+        }
+
+        // GET api/values/5
+        public OneDriveConnectorRequest Get(int id)
+        {
+            return files.ElementAtOrDefault(id);
+        }
+
+        // POST api/values
+        public void Post([FromBody]OneDriveConnectorRequest[] value)
+        {
+            files = value;
+        }
+    }
+}
---
+++ OneDriveService/Models/OneDriveConnectorRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OneDriveService.Models
+{
+    public class OneDriveConnectorRequest
+    {
+        public string FileName { get; set; }
+        public object FolderPath { get; set; }
+        public object LastModifiedUtc { get; set; }
+        public object FileSizeInBytes { get; set; }
+        public string FilePath { get; set; }
+    }
+}
